b9c448fbcba1b2d1e3d51e3e09f98fc54cbea266
Jade Rigby <contact@jaderigby.com>
2018-05-26 08:33:50 -0600

    adding polyfills for Array.forEach and NodeList.forEach

--- jquishy.js
+++ jquishy.js
@@ -1,24 +1,11 @@
 /*
+jQuishy
+-------------------------
 Author: Jade C. Rigby
 Date: 5/24/2018
-Version: 1.0.0
+Version: 1.1.0
 License: MIT
-
-jQuishy is now built for chaining!
-Also, added support for "first()"
-
-jQuishy is designed to take either a css string descriptor, a Nodelist, an HTMLCollection (gets converted to an Array), or a node object
-EXAMPLE:
-
-- string
-_$('#container ul li:first-child').addClass('active');
-
-- HTMLCollection
-_$(e.target.parent.children).removeClass('active');
-
-- node
-var myNode = document.getElementById('#container');
-_$(myNode).css('display', 'block');
+-------------------------
 */
 
 // ============== "Remove" Polyfill (IE9+) ==============
@@ -39,6 +26,85 @@ _$(myNode).css('display', 'block');
 		});
 	});
 })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+// ======================================================
+
+// ============== "Array forEach" Polyfill ==============
+//
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.io/#x15.4.4.18
+if (!Array.prototype.forEach) {
+
+  Array.prototype.forEach = function(callback/*, thisArg*/) {
+
+    var T, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling toObject() passing the
+    // |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get() internal
+    // method of O with the argument "length".
+    // 3. Let len be toUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If isCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let
+    // T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let k be 0.
+    k = 0;
+
+    // 7. Repeat while k < len.
+    while (k < len) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //    This is implicit for LHS operands of the in operator.
+      // b. Let kPresent be the result of calling the HasProperty
+      //    internal method of O with argument Pk.
+      //    This step can be combined with c.
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        // method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Call the Call internal method of callback with T as
+        // the this value and argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined.
+  };
+}
+// ======================================================
+
+// ============== "NodeList forEach" Polyfill ==============
+//
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
 // ======================================================
 
 var jQuishy = function(el) {
